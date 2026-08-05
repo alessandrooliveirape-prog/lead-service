7d9be985896d678ab3c417b87797f72d60fcf536
unknown <alessandrooliveirape@gmail.com>
2026-08-05 11:09:39 -0300

    fix: fallback PORT set to 3001 in index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,8 @@ import { AutomationPipelineService } from './services/automationPipelineService'
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
 
 app.use(cors());
 app.use(express.json());
